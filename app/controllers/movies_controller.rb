9691cc7277919f74bcb9afc4073032f93c9bd28c
Luis <lgomez6@uncc.edu>
2016-10-13 09:18:48 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.html.haml by default
   end
+  def new
+  @movie = Movie.new
+  # default: render 'new' template
+  end 
   # add below all other methods
 private
   def movie_params
